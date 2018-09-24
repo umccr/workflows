3aff6743f484be00a80e141b3e91ec2e040bfa32
Roman Valls Guimera <brainstorm@nopcode.org>
2018-09-24 15:58:32 +1000

    Minor LR-specific tweaks (file naming), add pre-called empty vcf as part of the setup

--- longranger/run.sh
+++ longranger/run.sh
@@ -6,29 +6,39 @@ sudo tee /opt/container/stack-wrapper.sh << 'END'
 #!/bin/bash
 set -euxo pipefail
 
+# install proper Perl-backed rename instead of the util-linux one
+# https://unix.stackexchange.com/questions/256412/backreferences-in-rename-regex
+sudo apt-get install rename
+
 DST_DIR="/mnt"
+CORES="40"
+MEM="100"
 
 echo "INPUT dataset: $S3_INPUT_DIR"
 echo "OUTPUT: ${DST_DIR}"
 
 cd ${DST_DIR} && mkdir -p input ref output
 
+echo "PULL dummy pre-called VCF to skip LR variant calling"
+wget https://raw.githubusercontent.com/umccr/workflows/master/longranger/pre-called.vcf -O ${DST_DIR}/ref/pre-called.vcf
+
 echo "PULL reference from S3 bucket"
-aws s3 sync --no-progress s3://umccr-umccrise-refdata-dev/10X/refdata-GRCh38-2.1.0.tar.gz ${DST_DIR}/ref
+aws s3 cp --no-progress s3://umccr-umccrise-refdata-dev/10X/refdata-GRCh38-2.1.0.tar.gz ${DST_DIR}/ref
 
 echo "UNPACK the PCGR reference dataset"
 tar xfz ${DST_DIR}/refdata-*.tar.gz --directory=${DST_DIR}/ref
 
-# XXX: Just fetch a particular dataset, not the 1.5TB of multiple patients?
-#echo "FETCH input dataset from S3 bucket"
-#aws s3 sync --no-progress s3://umccr-umccrise-dev/${S3_INPUT_DIR} ${DST_DIR}/${S3_INPUT_DIR}
+echo "FETCH input dataset from S3 bucket"
+aws s3 sync --no-progress s3://umccr-primary-data-dev/${S3_INPUT_DIR} ${DST_DIR}/${S3_INPUT_DIR}
+
+echo "CONFORM to LR input filename schema"
+rename 's/(.*)_(.*)_(.*)\.fastq\.gz/$1_$2_S1_L001_$3_001.fastq.gz/' *.fastq.gz
 
 echo "RUN LongRanger"
-time docker run --privileged -v $PWD/input:/data/input -v $PWD/ref/refdata-GRCh38-2.1.0:/data/refdata-GRCh38-2.1.0 -v $PWD/ref/pre-called.vcf:/data/pre-called.vcf -v $PWD/output:/data/output -c 95 umccr/longranger:2.2.2 wgs --id="output" --fastqs=/data/input --reference=/data/refdata-GRCh38-2.1.0 --jobmode=local --precalled=/data/pre-called.vcf --disable-ui --localcores=95 --localmem=350
+time docker run --privileged -v $PWD/input:/data/input -v $PWD/ref/refdata-GRCh38-2.1.0:/data/refdata-GRCh38-2.1.0 -v $PWD/ref/pre-called.vcf:/data/ref/pre-called.vcf -v $PWD/output:/data/output -c ${CORES} umccr/longranger:2.2.2 wgs --id="output" --fastqs=/data/input --reference=/data/refdata-GRCh38-2.1.0 --jobmode=local --precalled=/data/ref/pre-called.vcf --disable-ui --localcores=${CORES} --localmem=${MEM}
 
-# XXX: Just copy/report the resulting CSV for now since we only want the sample DNA nanograms?
-#echo "COPY the output to S3 bucket"
-#aws s3 cp ${S3_INPUT_DIR}-output.tar.gz s3://umccr-longranger-dev/
+echo "COPY the output to S3 bucket"
+aws s3 cp ${S3_INPUT_DIR}-output.tar.gz s3://umccr-primary-data-dev/
 END
 
 sudo chmod 755 /opt/container/stack-wrapper.sh
