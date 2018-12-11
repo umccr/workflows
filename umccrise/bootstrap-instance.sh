e0262fe6c3099afa321eea9803a52f917bc53297
Roman Valls Guimera <brainstorm@nopcode.org>
2018-12-11 13:15:38 +1100

    Relocate OS docker storage directory to bigger mountpoint (to accomodate pulling/uncompressing fat docker containers). /cc @vladsaveliev

--- umccrise/bootstrap-instance.sh
+++ umccrise/bootstrap-instance.sh
@@ -35,12 +35,17 @@ sudo mkfs.btrfs -f "$AWS_DEV"
 sudo echo -e "$AWS_DEV\t/mnt\tbtrfs\tdefaults\t0\t0" | sudo tee -a /etc/fstab
 sudo mount -a
 
+# Move docker storage to bigger volume
+sudo systemctl docker stop
+sudo mv /var/lib/docker /mnt/varlibdocker
+ln -sf /mnt/varlibdocker /var/lib/docker
+sudo systemctl docker start
+
 # Inject current AWS Batch underlying ECS cluster ID since the latter is dynamic. Match the computing environment with $STACK we are provisioning
 AWS_CLUSTER_ARN=$(aws ecs list-clusters --region "$AWS_REGION" --output json --query 'clusterArns' | jq -r .[] | grep "$STACK" | awk -F "/" '{ print $2 }')
 sudo sed -i "s/ECS_CLUSTER=\"default\"/ECS_CLUSTER=$AWS_CLUSTER_ARN/" /etc/default/ecs
 
 # Restart systemd/docker service
-
 sudo systemctl restart docker-container@ecs-agent.service
 
 # Now create a wrapper script to run the tool inside the job container
