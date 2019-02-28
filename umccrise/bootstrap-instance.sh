a721d84da41ac1573370c5ae245446e2ba34fbc9
Unknown <nick.clark@identitii.com>
2019-02-28 12:02:46 +1100

    Add support for m5 instance types

--- umccrise/bootstrap-instance.sh
+++ umccrise/bootstrap-instance.sh
@@ -3,8 +3,17 @@
 set -euxo pipefail
 
 export STACK="umccrise"
-export AWS_DEV="/dev/xvdb" # XXX: Hardcoded for now since instance metadata is not consistent between t2, m4 and m5 instances. See:
-# https://stackoverflow.com/questions/49891037/retrieve-correct-amazon-attached-ebs-device-from-instance-metadata-endpoint
+export INSTANCE_TYPE=$(curl -s http://169.254.169.254/latest/meta-data/instance-type/)
+### 
+
+if [[ $INSTANCE_TYPE =~ ^m5\..* ]]; then
+    export AWS_DEV="/dev/sdf" 
+elif [[ $INSTANCE_TYPE =~ ^m4\..* ]]; then
+    export AWS_DEV="/dev/xvdb" 
+else
+    echo -e "Detected unsupported instance type ${INSTANCE_TYPE}.\nWe'll give this a whirl anyway. Defaulting AWS_DEV to /dev/xvdb."
+    export AWS_DEV="/dev/xvdb" 
+fi
 
 # AWS instance introspection
 export AWS_AZ=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
@@ -15,8 +24,6 @@ export AWS_VOL_SIZE="500" # in GB
 
 # Create a 500GB ST1 volume and fetch its ID
 VOL_ID=$(sudo aws ec2 create-volume --region "$AWS_REGION" --availability-zone "$AWS_AZ" --encrypted --size "$AWS_VOL_SIZE" --volume-type "$AWS_VOL_TYPE" --tag-specifications 'ResourceType=volume,Tags=[{Key=Name,Value=batch}]' | jq -r .VolumeId)
-
-
 
 # Wait for the volume to become available (block) and then attach it to the instance
 aws ec2 wait volume-available --region "$AWS_REGION" --volume-ids "$VOL_ID" --filters Name=status,Values=available
@@ -32,9 +39,18 @@ aws ec2 modify-instance-attribute --region "$AWS_REGION" --instance-id "$AWS_INS
 # XXX: Find a better way to do this :/
 sleep 10
 
+#This is pretty ugly, but required (for now) to leverage the new m5 instance types 
+if [[ $INSTANCE_TYPE =~ ^m5\..* ]]; then
+    hdparm -z /dev/nvme1n1
+    mkfs.btrfs -f /dev/nvme1n1
+    sudo echo -e "/dev/nvme1n1\t/mnt\tbtrfs\tdefaults\t0\t0" | sudo tee -a /etc/fstab
+else
+    mkfs.btrfs -f "$AWS_DEV"
+    sudo echo -e "$AWS_DEV\t/mnt\tbtrfs\tdefaults\t0\t0" | sudo tee -a /etc/fstab
+fi
+
 # Format/mount
-sudo mkfs.btrfs -f "$AWS_DEV"
-sudo echo -e "$AWS_DEV\t/mnt\tbtrfs\tdefaults\t0\t0" | sudo tee -a /etc/fstab
+
 sudo mount -a
 
 # Hard purge docker (meta)data and move docker storage to bigger volume
