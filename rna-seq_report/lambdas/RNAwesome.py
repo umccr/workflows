3dd99e962cb3e1ca1e93904306bb14406ce5fa7b
Sehrish Kanwal <kanwals@unimelb.edu.au>
2019-09-05 12:50:01 +1000

    add s3 path to umccrised WGS results

--- rna-seq_report/lambdas/RNAwesome.py
+++ rna-seq_report/lambdas/RNAwesome.py
@@ -11,18 +11,19 @@ def lambda_handler(event, context):
     bucket = event["Records"][0]["s3"]["bucket"]["name"]
     obj = event["Records"][0]["s3"]["object"]["key"]
     data_dir = os.path.dirname(os.path.dirname(os.path.dirname(obj)))
+    data_wts_dir = os.path.dirname((os.path.dirname(obj))
     job_name = bucket + "---" + data_dir.replace('/', '_').replace('.', '_')
 
     # List objects in Patients result folder
     response = s3.list_objects(Bucket=bucket, Prefix=data_dir, Delimiter='/')
     # Extract Prefix for WGS results path - e.g. it'll be 'Patients/ID/WGS/' or 'Patients/ID/WTS/' (if WGS results doesn't exist)
-    result_WGS = json.dumps(response["CommonPrefixes"][0])
-    print(result_WGS)
+    result_WGS = json.dumps(response["CommonPrefixes"][0]["Prefix"]).strip('\"')
     # Check if WGS results exist
     if result_WGS.split(("/"))[-2] == 'WGS':
-        print('YES')
-        # TODO build complete path to umccrise
+        result_umccrise = s3.list_objects(Bucket=bucket, Prefix=result_WGS, Delimiter='/')
+        data_wgs_dir = json.dumps(result_umccrise["CommonPrefixes"][0]["Prefix"]).strip('\"')+"umccrised"
+        print(data_wgs_dir)
     else:
-        print("WGS results for the sample is not available")
+        print("WGS results for the sample are not available")
 
-    return bucket, obj, data_dir, job_name
+    return data_wts_dir, data_wgs_dir
