94e7d815e4ba36967bd1711c49fbeb2f33ed3d0d
ohofmann <ohofmann72@gmail.com>
2020-01-10 11:07:06 +1100

    Additional helper scripts added

---
+++ configurations/scripts/clean_bcbio.sh
@@ -0,0 +1,15 @@
+# Hard-coded project directory for now; don't want this to go off the rails
+for DIRECTORY in /g/data/gx8/projects/PROJECT/2019*/ ;
+do
+  BATCH=$(basename $DIRECTORY)
+  CLEANBATCH=${BATCH//./_}
+
+  cd $DIRECTORY/work/
+  rm -v bcbio-*
+  rm -vr bcbiotx/
+  rm -v pbspro_*
+  rm -v checkpoints_parallel/*.done
+  rm -v run.sh.*
+  echo '-----' >> log/bcbio-nextgen-debug.log
+
+done
