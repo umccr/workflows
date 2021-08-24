44dba8060d21ced147d8afd4b1da49e8d98a894d
ohofmann <ohofmann72@gmail.com>
2021-08-24 12:40:00 +1000

    Revised run scripts

--- configurations/run_umccrise.sh
+++ configurations/run_umccrise.sh
@@ -10,11 +10,11 @@
 export PATH=/opt/bin:/bin:/usr/bin:/opt/pbs/default/bin
 
 # Dev version
-source /g/data3/gx8/extras/umccrise_2019_Nov/load_umccrise.sh
+#source /g/data3/gx8/extras/umccrise_2019_Nov/load_umccrise.sh
 
 # Alpha version
 #source /g/data/gx8/extras/umccrise_017_2020_Jan/load_umccrise.sh
 
 # Production version
-#source /g/data/gx8/extras/umccrise/load_umccrise.sh
+source /g/data/gx8/extras/umccrise/load_umccrise.sh
 umccrise . -j 16 --cluster-auto --unlock
--- configurations/run_wts.sh
+++ configurations/run_wts.sh
@@ -7,5 +7,5 @@
 #PBS -l software=bcbio
 #PBS -l wd
 #PBS -l storage=gdata/gx8
-export PATH=/g/data3/gx8/local/development/bcbio/anaconda/bin:/g/data/gx8/local/development/bin:/opt/bin:/bin:/usr/bin:/opt/pbs/default/bin
+export PATH=/g/data3/gx8/local/current/bcbio/anaconda/bin:/g/data/gx8/local/current/bin:/opt/bin:/bin:/usr/bin:/opt/pbs/default/bin
 bcbio_nextgen.py ../config/bcbio_system_normalgadi.yaml ../config/WORKFLOW.yaml -n 24 -q normal -s pbspro -t ipython -r 'walltime=10:00:00;noselect;jobfs=100GB;storage=scratch/gx8+gdata/gx8' --retries 1 --timeout 900
